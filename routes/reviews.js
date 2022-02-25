8082c275959d2c1ad5ad5f2f21bd759351a09c37
Metin Ahmad <metinmayi@gmail.com>
2022-02-25 17:12:06 +0100

    Changed name from "Verification" to "Validation"

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const loginValidation = require("../validation/validation.js");
+//localhost:3000/reviews/
+router.get("/", (req, res) => {
+	res.send("You performed a GET request to the reviews.");
+});
 
-router.get("/", () => {});
+router.post("/add", async (req, res) => {});
 
 module.exports = router;
