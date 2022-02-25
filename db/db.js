556f38b51ffc1082e3bac9a39574bf53cb65bb95
Metin Ahmad <metinmayi@gmail.com>
2022-02-25 17:09:25 +0100

    Removed login and logout functions. They are not handled via Database

--- db/db.js
+++ db/db.js
@@ -1,8 +1,20 @@
-export const db = {
-	async login() {},
-	async logout() {},
+const dotenv = require("dotenv");
+dotenv.config();
+const { MongoClient } = require("mongodb");
+const client = new MongoClient(process.env.MONGODB_TOKEN);
+client.connect(async (err) => {
+	if (err) {
+		console.log(err);
+		return;
+	}
+	console.log("Database is connected");
+});
+
+const db = {
 	async addReview() {},
 	async editReview() {},
 	async deleteReview() {},
 	async setRating() {},
 };
+
+module.exports = db;
