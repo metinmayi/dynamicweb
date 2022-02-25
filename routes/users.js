8627f010c320c288edd15049aca26b32d6dca12c
Metin Ahmad <metinmayi@gmail.com>
2022-02-25 17:08:16 +0100

    Working login API endpoint. Needs cookie insertion

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const loginValidation = require("../verification/verification.js");
 
-router.get("/", () => {});
+//localhost:3000/users/
+router.get("/login", async (req, res) => {
+	const result = await loginValidation(req.body.username, req.body.password);
+	if (result.error) return res.status(400).send("Invalid payload");
+
+	//INSERT COOKIE HERE
+	res.send(req.body);
+});
 
 module.exports = router;
