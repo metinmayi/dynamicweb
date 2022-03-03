83c0557c181900790867fdf54c1c7d20cfe0a714
Metin Ahmad <metinmayi@gmail.com>
2022-03-03 20:25:59 +0100

    Changed register to take in a body parameter

--- validation/validation.js
+++ validation/validation.js
@@ -1,29 +1,29 @@
 const Joi = require("joi");
 
 const login = (username, password) => {
-  const login = {
-    username: username,
-    password: password,
-  };
-  const schema = new Joi.object({
-    username: Joi.string().min(1).max(12).required(),
-    password: Joi.string().required(),
-  });
-  return schema.validate(login);
+	const login = {
+		username: username,
+		password: password,
+	};
+	const schema = new Joi.object({
+		username: Joi.string().min(1).max(12).required(),
+		password: Joi.string().required(),
+	});
+	return schema.validate(login);
 };
 
-const register = (username, password, confirmPassword) => {
-  const register = {
-    username: username,
-    password: password,
-    confirmPassword: confirmPassword,
-  };
-  const schema = new Joi.object({
-    username: Joi.string().min(1).max(12).required(),
-    password: Joi.string().required(),
-    confirmPassword: Joi.string().required(),
-  });
-  return schema.validate(register);
+const register = (body) => {
+	const register = {
+		username: body.username,
+		password: body.password,
+		confirmPassword: body.confirmPassword,
+	};
+	const schema = new Joi.object({
+		username: Joi.string().min(1).max(12).required(),
+		password: Joi.string().required(),
+		confirmPassword: Joi.string().required(),
+	});
+	return schema.validate(register);
 };
 
 // const registerValidation = new Joi.object({
@@ -32,36 +32,36 @@ const register = (username, password, confirmPassword) => {
 // });
 
 const rating = (ratingObject) => {
-  const schema = new Joi.object({
-    id: Joi.string().required(),
-    rating: Joi.number().less(5).required(),
-  });
-  return schema.validate(ratingObject);
+	const schema = new Joi.object({
+		id: Joi.string().required(),
+		rating: Joi.number().less(5).required(),
+	});
+	return schema.validate(ratingObject);
 };
 
 const restaurant = (restaurantObject) => {
-  const schema = new Joi.object({
-    imageUrl: Joi.string().required(),
-    name: Joi.string().required(),
-    description: Joi.string().required(),
-    reviews: Joi.number().required(),
-    comments: Joi.array().required(),
-    totalRating: Joi.number().required(),
-    averageRating: Joi.number().required(),
-  });
+	const schema = new Joi.object({
+		imageUrl: Joi.string().required(),
+		name: Joi.string().required(),
+		description: Joi.string().required(),
+		reviews: Joi.number().required(),
+		comments: Joi.array().required(),
+		totalRating: Joi.number().required(),
+		averageRating: Joi.number().required(),
+	});
 
-  return schema.validate(restaurantObject);
+	return schema.validate(restaurantObject);
 };
 
 const edit = (data) => {
-  const schema = new Joi.object({
-    id: Joi.string().required(),
-    name: Joi.string(),
-    description: Joi.string(),
-    image: Joi.string(),
-  });
+	const schema = new Joi.object({
+		id: Joi.string().required(),
+		name: Joi.string(),
+		description: Joi.string(),
+		image: Joi.string(),
+	});
 
-  return schema.validate(data);
+	return schema.validate(data);
 };
 
 exports.register = register;
