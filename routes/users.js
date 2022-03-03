0a1f47cd5357bf0ceac86ab72db9d578fe2e99a2
Metin Ahmad <metinmayi@gmail.com>
2022-03-03 20:30:20 +0100

    Restructured users.js

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const { Db } = require("mongodb");
 const db = require("../db/db.js");
 const router = express.Router();
 const loginValidation = require("../validation/validation.js");
@@ -8,59 +7,47 @@ const jwt = require("jsonwebtoken");
 
 //localhost:3000/users/
 router.post("/register", async (req, res) => {
-  const result = await loginValidation.register(
-    req.body.username,
-    req.body.password,
-    req.body.confirmPassword
-  );
+	const result = await loginValidation.register(req.body);
+	if (result.error) return res.status(400).send(result.error.message);
 
-  const user = await db.getUserByUsername(req.body.username);
-  if (user) {
-    result.error;
-    return res.status(400).send("Username already exists");
-  } else if (req.body.password !== req.body.confirmPassword) {
-    console.log(req.body.password + req.body.confirmPassword);
-    return res.status(400).send("Passwords don't match");
-  } else {
-    const newUser = {
-      username: req.body.username,
-      hashedPassword: utils.hashPassword(req.body.password),
-    };
-    await db.addUser(newUser);
+	const user = await db.getUserByUsername(req.body.username);
+	if (user) return res.status(400).send("Username already exists");
 
-    res.redirect("login");
-  }
+	if (req.body.password !== req.body.confirmPassword) {
+		return res.status(400).send("Passwords don't match");
+	}
+	const newUser = {
+		username: req.body.username,
+		hashedPassword: utils.hashPassword(req.body.password),
+	};
+	await db.addUser(newUser);
+	res.status(200).redirect("login");
 });
 
 router.get("/login", (req, res) => {
-  res.render("users/login-register");
+	res.render("users/login-register");
 });
 
 router.post("/login", async (req, res) => {
-  // Ändrade till loginValidation.login annars hittar den inte funktionen?
-  const result = await loginValidation.login(
-    req.body.username,
-    req.body.password
-  );
+	// Ändrade till loginValidation.login annars hittar den inte funktionen?
+	const result = await loginValidation.login(
+		req.body.username,
+		req.body.password
+	);
+	if (result.error) return res.status(400).send(result.error.message);
 
-  console.log(req.body.username);
-  const user = await db.getUserByUsername(req.body.username);
-  console.log(user);
-  if (user && utils.comparePassword(req.body.password, user.hashedPassword)) {
-    const userData = { userId: user._id, username: req.body.username };
-    const accessToken = jwt.sign(userData, process.env.JWTSECRET);
-
-    res.cookie("token", accessToken);
-    res.redirect("/restaurants");
-  } else {
-    result.error;
-    return res.status(400).send("Invalid payload");
-  }
+	const user = await db.getUserByUsername(req.body.username);
+	if (user && utils.comparePassword(req.body.password, user.hashedPassword)) {
+		const userData = { userId: user._id, username: req.body.username };
+		const accessToken = jwt.sign(userData, process.env.JWTSECRET);
+		return res.cookie("token", accessToken).redirect("/restaurants");
+	}
+	return res.status(401).send("Incorrect username/password combination.");
 });
 
 router.post("/logout", async (req, res) => {
-  res.cookie("token", "", { maxAge: 0 });
-  res.redirect("/");
+	res.cookie("token", "", { maxAge: 0 });
+	res.redirect("/");
 });
 
 // router.get("/secret2", forceAuthorize, (req, res) => {
