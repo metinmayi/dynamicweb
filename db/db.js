3e38e1368a26067f5c95db1a110a1fc904f901bc
elinsjodin <elinsjodin@outlook.com>
2022-03-08 15:29:00 +0100

    Implemented error-messages in validation

--- db/db.js
+++ db/db.js
@@ -4,221 +4,222 @@ const { MongoClient, ObjectId } = require("mongodb");
 const { review, restaurant, edit } = require("../validation/validation");
 const client = new MongoClient(process.env.MONGODB_TOKEN);
 client.connect(async (err) => {
-	if (err) {
-		console.log(err);
-		return;
-	}
-	console.log("Database is connected");
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log("Database is connected");
 });
 
 const addUser = async (userObject) => {
-	try {
-		const result = await client
-			.db("grupparbete")
-			.collection("users")
-			.insertOne(userObject);
+  try {
+    const result = await client
+      .db("grupparbete")
+      .collection("users")
+      .insertOne(userObject);
 
-		return result;
-	} catch (error) {
-		return error.message;
-	}
+    return result;
+  } catch (error) {
+    return error.message;
+  }
 };
 
 const getUsers = async (specific = false) => {
-	try {
-		if (specific) {
-			return await client
-				.db("grupparbete")
-				.collection("users")
-				.findOne({ googleId: specific });
-		}
-		return await client
-			.db("grupparbete")
-			.collection("users")
-			.find({})
-			.toArray();
-	} catch (error) {
-		return error.message;
-	}
+  try {
+    if (specific) {
+      return await client
+        .db("grupparbete")
+        .collection("users")
+        .findOne({ googleId: specific });
+    }
+    return await client
+      .db("grupparbete")
+      .collection("users")
+      .find({})
+      .toArray();
+  } catch (error) {
+    return error.message;
+  }
 };
 
 const getUserByUsername = async (username) => {
-	try {
-		return await client
-			.db("grupparbete")
-			.collection("users")
-			.findOne({ username: username });
-	} catch (error) {
-		return error.message;
-	}
+  try {
+    return await client
+      .db("grupparbete")
+      .collection("users")
+      .findOne({ username: username });
+  } catch (error) {
+    return error.message;
+  }
 };
 
 //Function to get all restaurants
 
 const getRestaurants = async (specific = false) => {
-	try {
-		if (specific) {
-			return await client
-				.db("grupparbete")
-				.collection("restaurants")
-				.find({ creator: specific })
-				.toArray();
-		}
-		return await client
-			.db("grupparbete")
-			.collection("restaurants")
-			.find({})
-			.toArray();
-	} catch (error) {
-		return error.message;
-	}
+  try {
+    if (specific) {
+      return await client
+        .db("grupparbete")
+        .collection("restaurants")
+        .find({ creator: specific })
+        .toArray();
+    }
+    return await client
+      .db("grupparbete")
+      .collection("restaurants")
+      .find({})
+      .toArray();
+  } catch (error) {
+    return error.message;
+  }
 };
 
 //Function to get a specific restaurant
 const getSpecificRestaurant = async (id) => {
-	try {
-		const myId = ObjectId(id);
-		return await client
-			.db("grupparbete")
-			.collection("restaurants")
-			.findOne({ _id: myId });
-	} catch (error) {
-		return error.message;
-	}
+  try {
+    const myId = ObjectId(id);
+    return await client
+      .db("grupparbete")
+      .collection("restaurants")
+      .findOne({ _id: myId });
+  } catch (error) {
+    return error.message;
+  }
 };
 
 //Function to add a restaurant to the database
 const addRestaurant = async (reviewObject) => {
-	try {
-		const result = await client
-			.db("grupparbete")
-			.collection("restaurants")
-			.insertOne(reviewObject);
+  try {
+    const result = await client
+      .db("grupparbete")
+      .collection("restaurants")
+      .insertOne(reviewObject);
 
-		return result;
-	} catch (error) {
-		return error.message;
-	}
+    return result;
+  } catch (error) {
+    return error.message;
+  }
 };
 
 const editRestaurant = async (id, body) => {
-	const object = {};
-	if (body.name) object["name"] = body.name;
-	if (body.description) object["description"] = body.description;
-	try {
-		const mongoID = new ObjectId(id);
-		const editRestaurant = await client
-			.db("grupparbete")
-			.collection("restaurants")
-			.updateOne({ _id: mongoID }, { $set: object });
+  console.log(body);
+  const object = {};
+  if (body.name) object["name"] = body.name;
+  if (body.description) object["description"] = body.description;
+  try {
+    const mongoID = new ObjectId(id);
+    const editRestaurant = await client
+      .db("grupparbete")
+      .collection("restaurants")
+      .updateOne({ _id: mongoID }, { $set: object });
 
-		return editRestaurant;
-	} catch (error) {
-		return `${error.name}: ${error.message}`;
-	}
+    return editRestaurant;
+  } catch (error) {
+    return `${error.name}: ${error.message}`;
+  }
 };
 
 const deleteRestaurant = async (id) => {
-	try {
-		const mongoID = new ObjectId(id);
-		const deletedRestaurant = await client
-			.db("grupparbete")
-			.collection("restaurants")
-			.deleteOne({ _id: mongoID });
-		return deletedRestaurant.deletedCount > 0
-			? "Deleted the restaurant"
-			: "Couldn't find that restaurant";
-	} catch (error) {
-		return `${error.name}: ${error.message}`;
-	}
+  try {
+    const mongoID = new ObjectId(id);
+    const deletedRestaurant = await client
+      .db("grupparbete")
+      .collection("restaurants")
+      .deleteOne({ _id: mongoID });
+    return deletedRestaurant.deletedCount > 0
+      ? "Deleted the restaurant"
+      : "Couldn't find that restaurant";
+  } catch (error) {
+    return `${error.name}: ${error.message}`;
+  }
 };
 
 const setRating = async (id, rating) => {
-	console.log(id, rating);
-	try {
-		const mongoID = new ObjectId(id);
-		await client
-			.db("grupparbete")
-			.collection("restaurants")
-			.updateOne({ _id: mongoID }, [
-				{
-					$set: {
-						reviews: {
-							$add: ["$reviews", 1],
-						},
-						totalRating: {
-							$add: ["$totalRating", +rating],
-						},
-					},
-				},
-				{
-					$set: {
-						averageRating: {
-							$round: [{ $divide: ["$totalRating", "$reviews"] }, 2],
-						},
-					},
-				},
-			]);
-	} catch (error) {
-		return error.message;
-	}
+  console.log(id, rating);
+  try {
+    const mongoID = new ObjectId(id);
+    await client
+      .db("grupparbete")
+      .collection("restaurants")
+      .updateOne({ _id: mongoID }, [
+        {
+          $set: {
+            reviews: {
+              $add: ["$reviews", 1],
+            },
+            totalRating: {
+              $add: ["$totalRating", +rating],
+            },
+          },
+        },
+        {
+          $set: {
+            averageRating: {
+              $round: [{ $divide: ["$totalRating", "$reviews"] }, 2],
+            },
+          },
+        },
+      ]);
+  } catch (error) {
+    return error.message;
+  }
 };
 
 const forceAuthorize = (req, res, next) => {
-	const { token } = req.cookies;
+  const { token } = req.cookies;
 
-	if (token && jwt.verify(token, process.env.JWTSECRET)) {
-		// Inloggade
-		next();
-	} else {
-		// Utloggade
-		res.sendStatus(401);
-	}
+  if (token && jwt.verify(token, process.env.JWTSECRET)) {
+    // Inloggade
+    next();
+  } else {
+    // Utloggade
+    res.sendStatus(401);
+  }
 };
 
 const addComment = async (body) => {
-	try {
-		const mongoID = new ObjectId(body.id);
-		const addedComment = await client
-			.db("grupparbete")
-			.collection("restaurants")
-			.updateOne(
-				{ _id: mongoID },
-				{
-					$push: {
-						comments: { username: body.username, comment: body.comment },
-					},
-				}
-			);
-		return addedComment;
-	} catch (error) {
-		return error.message;
-	}
+  try {
+    const mongoID = new ObjectId(body.id);
+    const addedComment = await client
+      .db("grupparbete")
+      .collection("restaurants")
+      .updateOne(
+        { _id: mongoID },
+        {
+          $push: {
+            comments: { username: body.username, comment: body.comment },
+          },
+        }
+      );
+    return addedComment;
+  } catch (error) {
+    return error.message;
+  }
 };
 
 const addRating = async (body) => {
-	try {
-		const mongoID = new ObjectId(body.id);
-		const addedRating = await client
-			.db("grupparbete")
-			.collection("restaurants")
-			.updateOne({ _id: mongoID }, [
-				{
-					$set: {
-						reviews: {
-							$add: ["$reviews", 1],
-						},
-						totalRating: {
-							$add: ["$totalRating", body.rating],
-						},
-					},
-				},
-			]);
-		console.log(addedRating);
-		return addedRating;
-	} catch (error) {
-		return error.message;
-	}
+  try {
+    const mongoID = new ObjectId(body.id);
+    const addedRating = await client
+      .db("grupparbete")
+      .collection("restaurants")
+      .updateOne({ _id: mongoID }, [
+        {
+          $set: {
+            reviews: {
+              $add: ["$reviews", 1],
+            },
+            totalRating: {
+              $add: ["$totalRating", body.rating],
+            },
+          },
+        },
+      ]);
+    console.log(addedRating);
+    return addedRating;
+  } catch (error) {
+    return error.message;
+  }
 };
 exports.addUser = addUser;
 exports.getUserByUsername = getUserByUsername;
--- routes/restaurants.js
+++ routes/restaurants.js
@@ -7,114 +7,126 @@ const { getUniqueFilename } = require("../utils.js");
 
 //localhost:3000/restaurants/
 router.get("/", async (req, res) => {
-	const restaurants = await db.getRestaurants();
+  const restaurants = await db.getRestaurants();
 
-	res.render("home", { restaurants });
+  res.render("home", { restaurants });
 });
 
 router.get("/mypage", async (req, res) => {
-	const { token } = req.cookies;
-	const restaurants = await db.getRestaurants(res.locals.username);
-	console.log(restaurants);
+  const { token } = req.cookies;
+  const restaurants = await db.getRestaurants(res.locals.username);
+  console.log(restaurants);
 
-	if (token && jwt.verify(token, process.env.JWTSECRET)) {
-		res.render("restaurants/myPage", { restaurants });
-	} else {
-		res.send("Login failed!");
-	}
+  if (token && jwt.verify(token, process.env.JWTSECRET)) {
+    res.render("restaurants/myPage", { restaurants });
+  } else {
+    res.send("Login failed!");
+  }
 });
 
+// Här måste vi ändra för nu är man kvar på samma sida när man får fel-meddelandet men det står att 'id' måste vara en string
 router.post("/comment/:id", async (req, res) => {
-	const commentObject = {
-		id: req.params.id,
-		comment: req.body.comment,
-		username: res.locals.username,
-	};
-	const validateResult = await validation.addComment(commentObject);
-	if (validateResult.error)
-		return res.status(400).send(validateResult.error.message);
+  const commentObject = {
+    id: req.params.id,
+    comment: req.body.comment,
+    username: res.locals.username,
+  };
+  const validateResult = await validation.addComment(commentObject);
+  const restComment = await db.getSpecificRestaurant(req.params.id);
 
-	const addComment = await db.addComment(commentObject);
+  if (validateResult.error) {
+    restComment["error"] = validateResult.error.message;
+    restComment["differentUser"] = true;
+    return res.status(400).render("restaurants/review-single", restComment);
+  }
 
-	res.redirect("/");
+  await db.addComment(commentObject);
+
+  res.redirect("/restaurants/:id");
 });
 
+//Här måste vi ändra så att det går att lämna en rating utan fel + ändra till less(6) istället för 5 på rad 80 i validaiton.js
+//Rates a restaurant
 router.post("/rate/:id", async (req, res) => {
-	const commentObject = {
-		id: req.params.id,
-		rating: req.body.rating,
-	};
-	const validateResult = await validation.addRating(commentObject);
-	if (validateResult.error)
-		return res.status(400).send(validateResult.error.message);
+  const result = await validation.addRating(req.params.id, req.body.rating);
+  const restaurant = await db.getSpecificRestaurant(req.params.id);
 
-	const addComment = await db.setRating(req.params.id, req.body.rating);
-	console.log(addComment);
+  if (result.error) {
+    restaurant["error"] = result.error.message;
+    restaurant["differentUser"] = true;
+    return res.status(400).render("restaurants/review-single", restaurant);
+  }
+  await db.setRating(req.params.id, req.body.rating);
 
-	res.redirect("/");
+  res.redirect("/restaurants/:id");
 });
 
+// Här måste vi ändra så att man är kvar på samma sida när man får fel-meddelandet
 //Creates a new restaurant
 router.post("/mypage/add", async (req, res) => {
-	//Creates and object based on the request body.
-	const image = req.files.image;
-	const filename = getUniqueFilename(image.name);
-	const uploadPath = __dirname + "/../public/uploads/" + filename;
+  //Creates and object based on the request body.
+  if (!req.files) {
+    return res.status(400).render("restaurants/myPage", {
+      error: "Image is missing",
+    });
+  }
 
-	await image.mv(uploadPath);
+  const image = req.files.image;
+  const filename = getUniqueFilename(image.name);
+  const uploadPath = __dirname + "/../public/uploads/" + filename;
 
-	const restaurantObject = {
-		imageUrl: "/uploads/" + filename,
-		name: req.body.name,
-		description: req.body.description,
-		reviews: 0,
-		comments: [],
-		totalRating: 0,
-		averageRating: 0,
-		creator: res.locals.username,
-	};
+  await image.mv(uploadPath);
 
-	//Validates the object
-	const result = await validation.restaurant(restaurantObject);
-	//If the object validation returns an error, send the error to the user.
-	if (result.error) return res.status(400).send(result.error.message);
-	//If it does not return error, send the object to the database.
-	const addedReview = await db.addRestaurant(restaurantObject);
-	res.redirect(`/restaurants/${addedReview.insertedId}`);
+  const restaurantObject = {
+    imageUrl: "/uploads/" + filename,
+    name: req.body.name,
+    description: req.body.description,
+    reviews: 0,
+    comments: [],
+    totalRating: 0,
+    averageRating: 0,
+    creator: res.locals.username,
+  };
+
+  //Validates the object
+  const result = await validation.restaurant(restaurantObject);
+  //If the object validation returns an error, send the error to the user.
+  if (result.error)
+    return res.status(400).render("restaurants/myPage", {
+      error: result.error.message,
+    });
+  //If it does not return error, send the object to the database.
+  const addedReview = await db.addRestaurant(restaurantObject);
+  res.redirect(`/restaurants/${addedReview.insertedId}`);
 });
 
 // Single page
 router.get("/:id", async (req, res) => {
-	const restaurant = await db.getSpecificRestaurant(req.params.id);
-	if (restaurant.creator === res.locals.username) {
-		restaurant["sameUser"] = true;
-	} else {
-		restaurant["differentUser"] = true;
-	}
-	res.render("restaurants/review-single", restaurant);
-});
-
-//Rates a restaurant
-router.post("/rate/", async (req, res) => {
-	await db.setRating(req.body.id, req.body.rating);
-	const result = await validation.rating(req.body.mongoID, req.body.rating);
-	if (result.error) return res.status(400).send("Invalid Payload");
-	res.send("Good job");
+  const restaurant = await db.getSpecificRestaurant(req.params.id);
+  if (restaurant.creator === res.locals.username) {
+    restaurant["sameUser"] = true;
+  } else {
+    restaurant["differentUser"] = true;
+  }
+  res.render("restaurants/review-single", restaurant);
 });
 
 //Deletes a restaurant
 router.post("/delete/:id", async (req, res) => {
-	const result = await db.deleteRestaurant(req.params.id);
-	res.render("/restaurants/review-single");
+  await db.deleteRestaurant(req.params.id);
+  res.redirect("/restaurants/mypage");
 });
 //Edits a restaurant
-router.post("/edit", async (req, res) => {
-	const validationResult = await validation.edit(req.body);
-	if (validationResult.error) {
-		return res.send(validationResult.error.message);
-	}
-	const result = await db.editRestaurant(req.body);
-	res.send(result);
+router.post("/edit/:id", async (req, res) => {
+  const validationResult = await validation.edit(req.params.id, req.body);
+
+  if (validationResult.error)
+    return res.status(400).render("restaurants/review-single", {
+      error: validationResult.error.message,
+    });
+
+  await db.editRestaurant(req.params.id, req.body);
+  res.redirect("/restaurants/mypage");
 });
 
 module.exports = router;
